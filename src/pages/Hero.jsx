4ef5a5b832eaeb60e03e17036a9530369df457a1
satriazafarani <satriazafarani57@gmail.com>
2026-04-23 09:26:20 +0700

    added hero section

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,8 +1,36 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay, EffectFade } from "swiper/modules"
+import Slide1 from '../assets/hero/hero1.jpg'
+import Slide2 from '../assets/hero/hero2.jpg'
+import "swiper/css";
+import "swiper/css/effect-fade";
+
 const Hero = () => {
     return (
-        <>
-            <p>Testttt</p>
-        </>
+        <div className="w-full flex justify-center h-auto p-8">
+            <Swiper modules={[Autoplay, EffectFade]} effect="fade" autoplay={{ delay: 4000 }} loop className=" h-full">
+                <SwiperSlide>
+                    <div className="relative flex justify-center items-center">
+                        <img src={Slide1} className="w-full h-screen object-cover " />
+                        <div className="absolute inset-0 bg-black/30 flex flex-col gap-8 items-center justify-center ">
+                            <p className="text-lg font-semibold text-uppercase text-white">LOREM IPSUM DOLOR SIT</p>
+                            <p className="text-5xl text-white font-semibold">Lorem ipsum dolor sit amet.</p>
+                            <button className='bg-black w-40 h-10 font-bold text-sm text-white'>Shop Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="relative flex justify-center items-center">
+                        <img src={Slide2} className="w-full h-screen object-cover " />
+                        <div className="absolute inset-0 bg-black/30 flex flex-col gap-8 items-center justify-center">
+                            <p className="text-lg font-semibold text-white">LOREM IPSUM DOLOR SIT</p>
+                            <p className="text-5xl text-white font-semibold"> Walk in Comfort, Step in Style.</p>
+                            <button className='bg-black w-40 h-10 font-bold text-sm text-white'>Shop Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
     )
 }
 
