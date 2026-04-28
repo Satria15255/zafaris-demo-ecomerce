f9ebda092572640706270a61e88f9228e79a5763
satriazafarani <satriazafarani57@gmail.com>
2026-04-28 10:00:40 +0700

    added category section

--- src/pages/CallAction.jsx
+++ src/pages/CallAction.jsx
@@ -2,18 +2,23 @@ import actionImage from '../assets/hero/action.jpg'
 
 const CallAction = () => {
     return (
-        <div className='relative'>
-            <div className='p-6 h-120'>
-                <div className='h-full'>
-                    <img src={actionImage} className='w-full h-full object-cover object-center' />
-                </div>
-                <div className='absolute flex flex-col justify-center gap-7 inset-0 p-7 ml-8 font-sans'>
+        <div className='p-6 h-120 overflow-hidden '>
+            <div className='relative h-full overflow-hidden group'>
+
+                <div
+                    style={{ backgroundImage: `url(${actionImage})` }}
+                    className='absolute inset-0 bg-cover bg-center transition-transform duration-500 group-hover:scale-110'
+                ></div>
+
+                {/* Content */}
+                <div className='relative z-10 gap-7 flex flex-col justify-center h-full p-7 ml-8 font-sans'>
                     <p className='text-6xl max-w-lg'>Find Your Perfect Pair Faster</p>
                     <p className='text-sm'>Filter by Category and Size to Find the Perfect of Shoes</p>
                     <div>
                         <button className='bg-black text-white h-12 px-8'>Shop Now</button>
                     </div>
                 </div>
+
             </div>
         </div>
     )
--- src/pages/CategorySection.jsx
+++ src/pages/CategorySection.jsx
@@ -5,26 +5,42 @@ import casualCollection from '../assets/category/casual.png'
 
 const collection = [
     {
-        image: `${basketballCollection}`
+        image: `${basketballCollection}`,
+        title: "Basketball Collection",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempore itaque facere."
     },
     {
-        image: `${runningCollection}`
+        image: `${runningCollection}`,
+        title: "Running Collection",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempore itaque facere."
+
     },
     {
-        image: `${sneakersCollection}`
+        image: `${sneakersCollection}`,
+        title: "Sneakers Collection",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempore itaque facere."
     },
     {
-        image: `${casualCollection}`
+        image: `${casualCollection}`,
+        title: "Casual Collection",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempore itaque facere."
     },
 ]
 
 const CategoryCollection = () => {
     return (
-        <div className='h-screen w-full bg-[#FAFAFA]'>
+        <div className='h-auto py-12 px-4 w-full bg-[#FAFAFA]'>
             <div className='flex justify-around'>
                 {collection.map((c) => (
-                    <div className='w-1/5'>
-                        <img src={c.image} />
+                    <div className='w-80'>
+                        <div className='overflow-hidden'>
+                            <img src={c.image} className='hover:scale-110 transition duration-300' />
+                        </div>
+                        <div className='flex flex-col gap-5 mt-6'>
+                            <p className='text-lg font-semibold'>{c.title}</p>
+                            <p className='text-sm text-gray-500'>{c.desc}</p>
+                            <p className='underline pb-7 text-sm font-semibold'>SHOP NOW</p>
+                        </div>
                     </div>
                 ))}
             </div>
