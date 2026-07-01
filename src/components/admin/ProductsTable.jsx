37e0655268b8d31c10358dfac4d56b6355b956a0
satriazafarani <satriazafarani57@gmail.com>
2026-07-01 20:10:09 +0700

    update product tabel product admin pages: adedd id product

--- src/components/admin/ProductsTable.jsx
+++ src/components/admin/ProductsTable.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { FaEdit } from "react-icons/fa";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
 const ProductsTable = ({ products, onEdit, onDelete }) => {
   return (
-    <div className="w-full border-collapse ">
+    <div className="w-full border-collapse border border-gray-300">
       <table className="w-full table-fixed">
         <thead className="bg-gray-100 w-full">
           <tr>
@@ -24,21 +24,24 @@ const ProductsTable = ({ products, onEdit, onDelete }) => {
                 key={p._id}
                 className="border-b border-gray-300 w-full table-fixed "
               >
-                <td className="p-2 w-1/3 flex items-center">
-                  <img
-                    src={p.image}
-                    alt={p.name}
-                    className="w-16 h-16 object-cover mr-2 inline-block rounded-md"
-                  />
-                  <div>
-                    <p>{p.name}</p>
-                    <p className="text-xs text-gray-500">#{p._id}</p>
+                <td className="p-2  w-1/3">
+                  <div className="flex">
+                    <img
+                      src={p.image}
+                      alt={p.name}
+                      className="w-16 h-16 object-cover mr-2 inline-block rounded-md"
+                    />
+                    <p className="p-2 flex flex-col">
+                      {p.name}
+                      <span className="text-xs text-gray-400">#{p._id}</span>
+                    </p>
                   </div>
                 </td>
+
                 <td className="p-2">{p.brand}</td>
                 <td className="p-2">${p.price}</td>
                 <td className="p-2">{p.category}</td>
-                <td className=" space-x-2">
+                <td className="space-x-2">
                   <button
                     onClick={() => onEdit(p)}
                     className="text-gray-900 border border-gray-400 hover:bg-gray-900 hover:text-white transition duration-100 rounded-lg p-4"
@@ -47,9 +50,9 @@ const ProductsTable = ({ products, onEdit, onDelete }) => {
                   </button>
                   <button
                     onClick={() => onDelete(p._id)}
-                    className="text-gray-900 border border-black hover:bg-gray-900 hover:text-white transition duration-100 rounded-lg w-2/5 py-2"
+                    className="text-gray-900 border border-black hover:bg-gray-900 hover:text-white transition duration-100 rounded-lg p-4"
                   >
-                    delet
+                    <FaTrashAlt />
                   </button>
                 </td>
               </tr>
