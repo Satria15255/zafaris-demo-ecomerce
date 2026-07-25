d0614652118dd63be2dab4052aedd674b13408fe
satriazafarani <satriazafarani57@gmail.com>
2026-07-25 21:01:01 +0700

    update login & register pages

--- src/pages/client/Login.jsx
+++ src/pages/client/Login.jsx
@@ -32,23 +32,27 @@ const Login = () => {
             style={{ backgroundImage: `url(${background})` }}
         >
             <div className="inset-0  flex h-screen">
-                <div className="w-2/5 md:w-2/5 h-screen flex flex-col justify-center bg-white rounded-xl p-1 md:p-4 px-7">
-                    <div className="flex flex-col gap-2 items-center pb-4">
-                        <p className="text-lg md:text-2xl xl:text-5xl text-center font-bold mb-2 md:mb-4">
-                            Welcome Back!
-                        </p>
-                        <p className="text-sm text-gray-700 lorem21 max-w-sm text-center">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corrupti tempore itaque facere.
-                        </p>
+                <div className="w-2/5 md:w-2/5 h-screen flex flex-col justify-around items-center bg-white rounded-xl p-1 md:p-4 px-7">
+                    <div className="flex flex-col gap-2 items-center justify-around h-4/5 pb-4 pt-6">
+                        <div>
+                            <p className="text-lg md:text-2xl xl:text-5xl text-center font-bold mb-2 md:mb-4">
+                                Welcome Back!
+                            </p>
+                            <p className="text-sm text-gray-700 lorem21 max-w-sm text-center">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Corrupti tempore itaque
+                                facere.
+                            </p>
+                        </div>
+
                         <form
                             onSubmit={handleSubmit}
-                            className="space-y-2 md:space-y-2 flex px-3 flex-col justify-center w-100"
+                            className="space-y-2 md:space-y-2 flex px-3 flex-col justify-center w-130"
                         >
                             <label className="font-semibold">Email</label>
                             <input
                                 type="email"
-                                placeholder="Enter your email"
+                                placeholder="Enter Your Email"
                                 value={email}
                                 className="w-full text-sm md:text-lg rounded-lg p-2 border border-gray-300 rounded-2xl"
                                 onChange={(e) => setEmail(e.target.value)}
@@ -75,19 +79,19 @@ const Login = () => {
                                 Login
                             </button>
                         </form>
-                    </div>
-                    <div className="flex items-center justify-center mt-2 md:mt-4">
-                        <p className="text-center text-xs md:text-sm text-gray-500">
-                            Don't have an account?
-                        </p>
-                        <p
-                            onClick={() => {
-                                navigate("/register");
-                            }}
-                            className="text-gray-900 text-center text-xs md:text-sm mb-3 hover:underline font-bold"
-                        >
-                            Create Account
-                        </p>
+                        <div className="flex items-center justify-center mt-2 md:mt-4">
+                            <p className="text-center text-xs md:text-sm text-gray-500">
+                                Don't have an account?
+                            </p>
+                            <p
+                                onClick={() => {
+                                    navigate("/register");
+                                }}
+                                className="text-gray-900 text-center text-xs md:text-sm mb-3 hover:underline font-bold"
+                            >
+                                Create Account
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/client/Register.jsx
+++ src/pages/client/Register.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { register } from "@/api/Api";
 import background from "@/assets/heroSection/hero12.jpg";
+import { IoIosHome } from "react-icons/io";
 
 const Register = () => {
     const [name, setName] = useState("");
@@ -27,63 +28,71 @@ const Register = () => {
             className="fixed inset-0 z-50 bg-cover bg-center"
             style={{ backgroundImage: `url(${background})` }}
         >
-            <div className="inset-0 bg-black/50 flex items-center justify-center h-screen">
-                <div className="w-4/5 md:w-2/5 h-auto border bg-white rounded-xl p-2 md:p-4 mx-auto mt-10">
-                    <button
-                        onClick={() => {
-                            navigate("/");
-                        }}
-                        className="text-gray-500 -mt-6 text-lg hover:text-gray-700 text-xl font-bold top-0"
-                    >
-                        ×
-                    </button>
-                    <p className="text-lg md:text-2xl text-center font-bold mb-2 md:mb-4">
-                        Register
-                    </p>
-                    <form
-                        onSubmit={handleSubmit}
-                        className="space-y-2 md:space-y-4 flex flex-col justify-center"
-                    >
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            value={name}
-                            className="w-full text-sm md:text-lg rounded-lg p-2 border"
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            className="w-full text-sm md:text-lg rounded-lg p-2 border"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            className="w-full text-sm md:text-lg rounded-lg p-2 border"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <button
-                            type="submit"
-                            className="text-sm md:text-lg border border-black hover:bg-gray-900 hover:text-white transition duration-100 px-4 py-2 rounded"
+            <div className="inset-0  flex h-screen">
+                <div className="w-2/5 md:w-2/5 h-screen flex flex-col justify-around bg-white rounded-xl p-1 md:p-4 px-7">
+                    <div className="flex flex-col gap-2 justify-around items-center gap-5 pb-4">
+                        <div className="flex flex-col gap-2">
+                            <p className="text-lg md:text-2xl xl:text-5xl text-center font-bold mb-2 md:mb-4">
+                                Get Started It
+                            </p>
+                            <p className="text-sm text-gray-700 lorem21 max-w-sm text-center">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Corrupti tempore itaque
+                                facere.
+                            </p>
+                        </div>
+                        <form
+                            onSubmit={handleSubmit}
+                            className="space-y-2 md:space-y-4 flex flex-col justify-center w-140"
                         >
-                            Register
-                        </button>
-                    </form>
-                    <div className="flex flex-col items-center mt-2 md:mt-4 ">
-                        <p className="text-center text-xs md:text-sm">
-                            Already have an account?
-                        </p>
-                        <button
-                            onClick={() => {
-                                navigate("/login");
-                            }}
-                            className="text-gray-900 text-center hover:underline mb-3 text-xs md:text-sm"
-                        >
-                            Login
-                        </button>
+                            <label className="font-semibold">Full Name</label>
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                value={name}
+                                className="w-full text-sm md:text-lg rounded-lg p-2 border border-gray-300"
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <label className="pt-4 font-semibold">Email</label>
+
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                className="w-full text-sm md:text-lg rounded-lg p-2 border border-gray-300"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <label className="pt-4 font-semibold">
+                                Password
+                            </label>
+
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                className="w-full text-sm md:text-lg rounded-lg p-2 border  border-gray-300"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button
+                                type="submit"
+                                className="text-sm md:text-lg border border-gray-300 bg-gray-900 text-white hover:bg-white hover:text-black transition duration-100 px-4 py-2 rounded-2xl"
+                            >
+                                Register
+                            </button>
+                        </form>
+                        <div className="flex items-center justify-center mt-2 md:mt-4 ">
+                            <p className="text-center text-xs md:text-sm text-gray-500">
+                                Already have an account?
+                            </p>
+                            <p
+                                onClick={() => {
+                                    navigate("/login");
+                                }}
+                                className="text-gray-900 text-center hover:underline mb-3 text-xs md:text-sm"
+                            >
+                                Login
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
