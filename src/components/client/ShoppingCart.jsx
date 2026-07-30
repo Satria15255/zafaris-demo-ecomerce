7f5b8924be4fc337b9f9b35a4188faba28e402cb
satriazafarani <satriazafarani57@gmail.com>
2026-07-30 20:33:36 +0700

    update sidebar cart

--- src/components/client/ShoppingCart.jsx
+++ src/components/client/ShoppingCart.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { useCart } from "@/context/CartContext";
 import { useAuth } from "@/context/AuthContext";
 import { TbBasketQuestion } from "react-icons/tb";
+import { FaMinus } from "react-icons/fa";
+import { LuMinus, LuPlus } from "react-icons/lu";
 
 const ShoppingCart = ({ closeShoppingCart }) => {
     const { cart, removeCartItems, updateQty, totalPrice } = useCart();
@@ -47,20 +49,6 @@ const ShoppingCart = ({ closeShoppingCart }) => {
                                 </div>
                             ) : (
                                 <table className="w-full border-collapse">
-                                    <thead className="hidden md:table-header-group">
-                                        <tr className="border-b">
-                                            <th className="text-left text-xs pb-2">
-                                                PRODUCT
-                                            </th>
-                                            <th className="text-center text-xs pb-2">
-                                                QUANTITY
-                                            </th>
-                                            <th className="text-right text-xs pb-2">
-                                                TOTAL
-                                            </th>
-                                        </tr>
-                                    </thead>
-
                                     <tbody>
                                         {cart.map((item) => {
                                             return (
@@ -69,7 +57,7 @@ const ShoppingCart = ({ closeShoppingCart }) => {
                                                     className="border-b border-gray-300"
                                                 >
                                                     {/* Produk */}
-                                                    <td className="flex items-center py-4 px-2">
+                                                    <td className="flex items-center py-2">
                                                         <img
                                                             src={
                                                                 item.productId
@@ -79,112 +67,120 @@ const ShoppingCart = ({ closeShoppingCart }) => {
                                                                 item.productId
                                                                     .name
                                                             }
-                                                            className="w-25 h-25 md:w-20 md:h-20 lg:w-20 lg:h-20 object-cover rounded mr-1 md:mr-4"
+                                                            className="w-25 h-25 md:w-20 md:h-20 lg:w-25 lg:h-25 object-cover rounded mr-1 md:mr-4"
                                                         />
-                                                        <div>
-                                                            <p className="text-sm md:text-lg text-sm max-w-sm font-medium">
-                                                                {
-                                                                    item
-                                                                        .productId
-                                                                        .name
-                                                                }
-                                                            </p>
-                                                            <p className="text-xs lg:text-sm text-gray-600 font-semibold">
-                                                                Size:{" "}
-                                                                {item.size}
-                                                            </p>
-                                                            <div className="flex gap-2 items-center">
-                                                                {item.discountPercent >
-                                                                    0 && (
-                                                                    <div className="flex gap-2">
-                                                                        <p className="text-sm">
-                                                                            $
-                                                                            {
-                                                                                item.discountPercent
-                                                                            }
-                                                                            OFF
-                                                                        </p>
-                                                                        <p className="text-sm line-through">
-                                                                            $
-                                                                            {
-                                                                                item.price
-                                                                            }
-                                                                            .00{" "}
-                                                                        </p>
-                                                                    </div>
-                                                                )}
-                                                                <p className="text-yellow-500 text-sm text-lg font-semibold">
-                                                                    $
+
+                                                        <div className="flex flex-col justify-around gap-2  w-full h-full">
+                                                            <div>
+                                                                <p className="text-sm md:text-lg text-sm max-w-sm font-medium">
                                                                     {
-                                                                        item.finalPrice
+                                                                        item
+                                                                            .productId
+                                                                            .name
                                                                     }
-                                                                    .00
                                                                 </p>
+                                                            </div>
+                                                            <div className="flex gap-8">
+                                                                <p className="text-xs lg:text-sm text-gray-600 font-semibold">
+                                                                    Size:{" "}
+                                                                    {item.size}
+                                                                </p>
+                                                                <div className="flex gap-2 items-center">
+                                                                    {item.discountPercent >
+                                                                        0 && (
+                                                                        <div className="flex gap-2">
+                                                                            <p className="text-sm">
+                                                                                $
+                                                                                {
+                                                                                    item.discountPercent
+                                                                                }
+                                                                                OFF
+                                                                            </p>
+                                                                            <p className="text-sm line-through">
+                                                                                $
+                                                                                {
+                                                                                    item.price
+                                                                                }
+                                                                                .00{" "}
+                                                                            </p>
+                                                                        </div>
+                                                                    )}
+                                                                    <p className="text-yellow-500 text-sm text-lg font-semibold">
+                                                                        $
+                                                                        {
+                                                                            item.finalPrice
+                                                                        }
+                                                                        .00
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+
+                                                            <div className="flex justify-between ">
+                                                                <div className="flex items-center justify-center rounded-sm border border-gray-200">
+                                                                    <button
+                                                                        className="text-xs lg:text-sm xl:text-lg text-gray-400 hover:text-[#0C0C0C] transition duration-100 px-2 py-1   rounded"
+                                                                        onClick={() =>
+                                                                            updateQty(
+                                                                                item
+                                                                                    .productId
+                                                                                    ._id,
+                                                                                item.size,
+                                                                                item.quantity -
+                                                                                    1,
+                                                                            )
+                                                                        }
+                                                                    >
+                                                                        <LuMinus />
+                                                                    </button>
+                                                                    <span className="text-xs lg:text-sm px-2 md:px-4">
+                                                                        {
+                                                                            item.quantity
+                                                                        }
+                                                                    </span>
+                                                                    <button
+                                                                        className="text-xs lg:text-sm px-2 text-gray-400 hover:text-[#0C0C0C] transition duration-100 py-1   rounded"
+                                                                        onClick={() =>
+                                                                            updateQty(
+                                                                                item
+                                                                                    .productId
+                                                                                    ._id,
+                                                                                item.size,
+                                                                                item.quantity +
+                                                                                    1,
+                                                                            )
+                                                                        }
+                                                                    >
+                                                                        <LuPlus />
+                                                                    </button>
+                                                                </div>
+                                                                <button
+                                                                    onClick={() =>
+                                                                        removeCartItems(
+                                                                            item
+                                                                                .productId
+                                                                                ._id,
+                                                                            item.size,
+                                                                        )
+                                                                    }
+                                                                    className="text-xs md:text-sm text-gray-500 hover:text-red-500 md:mt-1"
+                                                                >
+                                                                    Remove
+                                                                </button>
                                                             </div>
                                                         </div>
                                                     </td>
 
                                                     {/* Quantity */}
-                                                    <td className="flex-col items-center text-center md:table-cell">
-                                                        <div className="flex items-center justify-center">
-                                                            <button
-                                                                className="text-xs lg:text-sm px-2 py-1 border rounded"
-                                                                onClick={() =>
-                                                                    updateQty(
-                                                                        item
-                                                                            .productId
-                                                                            ._id,
-                                                                        item.size,
-                                                                        item.quantity -
-                                                                            1,
-                                                                    )
-                                                                }
-                                                            >
-                                                                -
-                                                            </button>
-                                                            <span className="text-xs lg:text-sm px-2 md:px-4">
-                                                                {item.quantity}
-                                                            </span>
-                                                            <button
-                                                                className="text-xs lg:text-sm px-2 py-1 border rounded"
-                                                                onClick={() =>
-                                                                    updateQty(
-                                                                        item
-                                                                            .productId
-                                                                            ._id,
-                                                                        item.size,
-                                                                        item.quantity +
-                                                                            1,
-                                                                    )
-                                                                }
-                                                            >
-                                                                +
-                                                            </button>
-                                                        </div>
-                                                        <button
-                                                            onClick={() =>
-                                                                removeCartItems(
-                                                                    item
-                                                                        .productId
-                                                                        ._id,
-                                                                    item.size,
-                                                                )
-                                                            }
-                                                            className="text-xs md:text-sm text-gray-500 hover:text-red-500 md:mt-1"
-                                                        >
-                                                            Remove
-                                                        </button>
-                                                    </td>
 
                                                     {/* Total Harga per Item */}
-                                                    <td>
+                                                    {/*<td>
                                                         <div className="flex items-center text-lg justify-center text-right text-yellow-500 font-semibold  hidden md:block">
                                                             $
                                                             {item.finalPrice *
                                                                 item.quantity}
                                                             .00
                                                         </div>
-                                                    </td>
+                                                    </td>*/}
                                                 </tr>
                                             );
                                         })}
