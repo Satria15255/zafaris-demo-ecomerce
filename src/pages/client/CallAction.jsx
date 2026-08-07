71515800ef8975ac3ab684aae6bbfb1d73e919a2
satriazafarani <satriazafarani57@gmail.com>
2026-08-07 20:35:50 +0700

    update responsiv call action section mobile viewport

--- src/pages/client/CallAction.jsx
+++ src/pages/client/CallAction.jsx
@@ -5,27 +5,27 @@ const CallAction = () => {
     const navigate = useNavigate();
     return (
         <div className="p-3 md:p-6 xl:p-2 w-full xl:max-w-7xl h-auto overflow-hidden ">
-            <div className="relative h-full md:h-90 lg:h-120 xl:h-120 overflow-hidden group">
+            <div className="relative h-fullh-50 md:h-90 lg:h-120 xl:h-120 overflow-hidden group">
                 <div
                     style={{ backgroundImage: `url(${actionImage})` }}
                     className="absolute inset-0 bg-cover bg-center transition-transform duration-500 group-hover:scale-110"
                 ></div>
 
                 {/* Content */}
-                <div className="relative z-10 gap-7 md:gap-4 lg:gap-5 flex flex-col  justify-center h-full p-7  lg:ml-8 font-sans">
-                    <p className="text-5xl lg:text-6xl font-montserrat">
+                <div className="relative z-10 gap-2 md:gap-4 lg:gap-5 flex flex-col  justify-center h-50 p-7  lg:ml-8 font-sans">
+                    <p className="text-xl md:text-5xl lg:text-6xl font-montserrat">
                         Find Your Perfect <br /> Pair Faster
                     </p>
-                    <p className="text-md max-w-xs lg:max-w-sm hidden md:flex font-ysabeau">
-                        Filter by Category and Size to Find the Perfect of
-                        Shoes.
+                    <p className="text-[9px] text-md max-w-xs leading-tight lg:max-w-sm flex font-ysabeau">
+                        Filter by Category and Size <br /> to Find the Perfect
+                        of Shoes.
                     </p>
                     <div>
                         <button
                             onClick={() => navigate("/products")}
-                            className="bg-[#0C0C0C] text-white text-sm h-12 px-8 font-ysabeau"
+                            className="bg-[#0C0C0C] text-white text-[8px] md:text-sm py-1 md:h-12 px-4 md:px-8 font-ysabeau"
                         >
-                            SHOP NOW
+                            Shop Now
                         </button>
                     </div>
                 </div>
