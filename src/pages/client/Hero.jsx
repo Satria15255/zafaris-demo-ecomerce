f05a89e0c4ca1c867597e57d44cee8984cdc1912
satriazafarani <satriazafarani57@gmail.com>
2026-08-15 23:38:57 +0700

    update padding & fontn size hero section on tablet media query

--- src/pages/client/Hero.jsx
+++ src/pages/client/Hero.jsx
@@ -10,17 +10,17 @@ const Hero = () => {
     const navigate = useNavigate();
 
     return (
-        <div className=" px-2   lg:p-4   pt-15 md:pt-15 lg:pt-17 md:mt-16 flex justify-center w-full xl:max-w-8xl h-full xl:h-auto">
-            <div className="relative flex justify-end items-center w-100 md:w-full h-50 xl:h-120">
+        <div className=" px-2   md:p-4   pt-15  md:pt-12 lg:pt-17 md:mt-17 flex justify-center w-full xl:max-w-8xl h-full xl:h-auto">
+            <div className="relative flex justify-end items-center w-100 md:w-full h-50 md:h-100 xl:h-120  ">
                 <img
                     src={Slide1}
-                    className="w-full h-full md:h-100 lg:h-screen xl:h-full object-cover object-center rounded-lg "
+                    className="w-full h-full   lg:h-screen xl:h-full object-cover object-center rounded-lg "
                 />
                 <div className="absolute inset-0 flex flex-col md:gap-4 lg:items-start p-4 xl:p-5 w-full lg:w-2/3  justify-center ">
                     <p className="text-[6px] md:text-sm font-montserrat text-white">
                         NEW ARRIVAL
                     </p>
-                    <p className="text-2xl md:text-4xl lg:text-7xl xl:text-7xl font-montserrat text-white ">
+                    <p className="text-2xl md:text-5xl lg:text-7xl xl:text-7xl font-montserrat text-white ">
                         New Collection <br /> Just Landed
                     </p>
                     <p className="text-[6px] leading-tight py-2 md:text-sm lg:text-lg text-white font-ysabeau">
@@ -30,13 +30,13 @@ const Hero = () => {
                     <div className="flex gap-1 md:gap-3 w-1/2 lg:w-full">
                         <button
                             onClick={() => navigate("/products")}
-                            className="bg-black w-1/3 md:px-5  md:h-10  text-[6px] md:text-[8px] lg:text-xs text-white hover:border border-white hover:bg-transparent transition duration-300"
+                            className="bg-black w-1/3 md:w-1/2 md:px-5  md:h-10  text-[6px] md:text-[8px] lg:text-xs text-white hover:border border-white hover:bg-transparent transition duration-300"
                         >
                             SHOP NOW
                         </button>
                         <button
                             onClick={() => navigate("/products")}
-                            className="bg-transparent hidden md:flex items-center justify-center w-1/3 md:px-5  md:h-10  text-[6px] md:text-xs  border border-white text-white hover:border-none hover:bg-black hover:text-white transition duration-300"
+                            className="bg-transparent hidden md:flex items-center justify-center w-1/3 md:w-1/2 md:px-5  md:h-10 text-[6px] md:text-[7px] lg:text-xs  border border-white text-white hover:border-none hover:bg-black hover:text-white transition duration-300"
                         >
                             EXPLORE COLLECTION
                         </button>
