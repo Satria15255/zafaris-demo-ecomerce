84f0978abfe40081ed8f91751698711a3cf8558d
satriazafarani <satriazafarani57@gmail.com>
2026-09-09 11:10:33 +0700

    fixing a few button miss function at discount calss

--- src/pages/public/home/sections/Discount.jsx
+++ src/pages/public/home/sections/Discount.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 const OnSale = () => {
+    const navigate = useNavigate();
     return (
         <main className=" mt-6 flex justify-center w-full h-auto">
             <div className="w-100 md:w-full xl:max-w-7xl h-50 md:h-60 lg:h-80 flex  bg-[#0C0C0C] ">
@@ -22,7 +23,10 @@ const OnSale = () => {
                     <p className="text-lg md:text-4xl lg:text-5xl xl:text-6xl">
                         30% off every day for <br /> our dream products
                     </p>
-                    <button className="py-1 lg:py-2 text-[7px] md:text-xs lg:text-lg px-4 rounded-lg text-black w-1/3 lg:w-1/5 bg-white border border-gray-300">
+                    <button
+                        onClick={() => navigate("/products")}
+                        className="py-1 lg:py-2 text-[7px] md:text-xs lg:text-lg px-4 rounded-lg text-black w-1/3 lg:w-1/5 bg-white border border-gray-300"
+                    >
                         Get Now
                     </button>
                 </header>
